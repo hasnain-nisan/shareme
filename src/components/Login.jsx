75ff17d592d5da970ddf78536abf9e5c1e9f5b29
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-04 19:36:56 +0600

    Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import video from '../assets/video.mp4'
-import {SiPhotopea} from 'react-icons/si'
+import { SiPhotopea } from "react-icons/si";
+import {FcGoogle} from 'react-icons/fc'
+import GoogleLogin from "react-google-login";
 
 const Login = () => {
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    };
+
     return (
       <div className="flex flex-col justify-start items-center h-screen">
         <div className="relative h-full w-full">
@@ -17,8 +24,26 @@ const Login = () => {
           />
           <div className="absolute flex flex-col items-center justify-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
             <div className="p-5 flex items-center">
-              <SiPhotopea className="text-white w-20 h-14"/>
+              <SiPhotopea className="text-white w-20 h-14" />
               <p className="text-white text-3xl font-bold">Shareme</p>
+            </div>
+
+            <div className="shadow-2xl">
+              <GoogleLogin
+                clientId=""
+                render={(renderProps) => (
+                  <button
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none ml-2"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FcGoogle className="mr-4" /> Sign in with google
+                  </button>
+                )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single_host_origin"
+              />
             </div>
           </div>
         </div>
