87918058560552a72d04b9e9a06538922265a87d
hasnain-nisan <you@example.com>
2022-01-06 23:37:31 +0600

    Started designinng

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import {Link, Route, Routes} from 'react-router-dom'
+import {Sidebar, UserProfile} from '../components'
+import {client} from '../client'
+import Pins from './Pins'
+import { SiPhotopea } from "react-icons/si";
+import {BsMenuButtonWideFill} from "react-icons/bs"
+
 
 const Home = () => {
+
+    const [toggleSidebar, setToggleSidebar] = useState(false)
+
     return (
-        <div>
-            HOme
+        <div className="flex flex-col md:flex-row bg-gray-50 h-screen transition-height duration-75 ease-out">
+            <div className="hidden md:flex h-screen flex-initial">
+                <Sidebar/>
+            </div>
+            <div className="flex md:hidden flex-row">
+                <BsMenuButtonWideFill fontSize={30} className="cursor-pointer" onCLick={() => setToggleSidebar(true)}/>
+            </div>
         </div>
     )
 }
