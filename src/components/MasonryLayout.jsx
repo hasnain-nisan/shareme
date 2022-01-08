1b786f2ab23cb22729f9023867c7d2e6bb04f605
hasnain-nisan <you@example.com>
2022-01-08 23:59:13 +0600

    Pin done

--- src/components/MasonryLayout.jsx
+++ src/components/MasonryLayout.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Masonry from 'react-masonry-css'
+import Pin from './Pin'
 
-const MasonryLayout = () => {
+const breakpointObj = {
+    default: 4,
+    3000: 6,
+    2000: 5,
+    1500: 4,
+    1200: 3,
+    1000: 2,
+    500: 1,
+}
+
+const MasonryLayout = ({pins}) => {
     return (
         <div>
-            Masonry
+            <Masonry className="flex animate-slide-fwd" breakpointCols={breakpointObj}>
+                {pins?.map((item) => <Pin key={item.id} item={item} className="w-max"/>)}
+            </Masonry>
         </div>
     )
 }
