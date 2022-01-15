982e7d47f95de6e48246f28440edfc9cd1a256b8
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-15 19:17:13 +0600

    user profile compnent

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {AiOutlineLogout} from 'react-icons/ai'
+import {useParams, useNavigate} from 'react-router-dom'
+import {GoogleLogout} from 'react-google-login'
+
+import {client} from '../client'
+import {userCreatedPinsQuery, userQuery, userSavedPinsQuery} from '../utils/data'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
+
 
 const UserProfile = () => {
+
+    const [user, setUser] = useState(null)
+    const [pins, setPins] = useState(null)
+    const [text, setText] = useState('Created')
+    const [activeBtn, setActiveBtn] = useState('created')
+    const navigate = useNavigate()
+    const {userId} = useParams
+
+    if(!user){
+        return <Spinner message={"Loading profile"}/>
+    }
+
     return (
         <div>
             User Profile
